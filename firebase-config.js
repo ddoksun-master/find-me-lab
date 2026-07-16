207e75ef280a817fbbc5a856ad2134b1a271b8af
ddoksun-master <wocsyy@gmail.com>
2026-07-16 15:25:30 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase project config (ME:LAB · lodestar-feaba)
+window.firebaseConfig = {
+  apiKey: "AIzaSyA6A6Piyk4IeCmH84l1SC6oykRROfMeWxs",
+  authDomain: "lodestar-feaba.firebaseapp.com",
+  projectId: "lodestar-feaba",
+  storageBucket: "lodestar-feaba.firebasestorage.app",
+  messagingSenderId: "515308275908",
+  appId: "1:515308275908:web:337495345db87914bee191",
+  measurementId: "G-MJ9GSSV0LK"
+};
